87e8119034d94f45af04d098605dfdf25916a8c5
paunov0000 <legendarygriefer96@gmail.com>
2024-02-06 02:04:38 +0200

    moving files

--- WebStore/WebStore.Core/Model/Product/ProductViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebStore.Core.Model.Product
-{
-    public class ProductViewModel : ProductAddViewModel
-    {
-        public Guid Id { get; set; }
-        public int SoldCount { get; set; }
-    }
-}
--- WebStore/WebStore.Infrastructure/Data/Entities/ApplicationRole.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace WebStore.Infrastructure.Data.Entities
-{
-    public class ApplicationRole : IdentityRole<Guid>
-    {
-    }
-}
