8b0c84cd4f85067caa969dc8dc6fadc7de32a8f4
paunov0000 <legendarygriefer96@gmail.com>
2024-02-28 20:47:18 +0200

    added unit tests project

--- WebStore.Infrastructure/obj/Debug/net8.0/WebStore.Infrastructure.AssemblyInfo.cs
+++ WebStore.Infrastructure/obj/Debug/net8.0/WebStore.Infrastructure.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e78d36298c43d62510405a5fc06a49f984392a11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f154947bcaa52bc333e7b63fcaff6c65c1151bf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebStore.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
