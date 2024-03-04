40dfd9a9258b228a2fd8679f984bf2c4353bd8db
paunov0000 <legendarygriefer96@gmail.com>
2024-03-05 01:19:50 +0200

    Added model to display on the Admin/Product/Index page which will be a table with all of the products

---
+++ WebStore.Core/Model/Admin/Product/ProductTableModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Infrastructure.Data.Entities;
+
+namespace WebStore.Core.Model.Admin.Product
+{
+    public class ProductTableModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Manufacturer { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public int SoldCount { get; set; }
+
+        [ForeignKey(nameof(ProductCategory))]
+        public Guid ProductCategoryId { get; set; }
+
+        public ProductCategory ProductCategory { get; set; } = null!;
+    }
+}
