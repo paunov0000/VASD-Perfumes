3d0f58f62cea436b73a07c5b0a6f77f6dc4b0732
paunov0000 <legendarygriefer96@gmail.com>
2024-03-05 01:18:24 +0200

    Added Admin Product Service and its' interface

---
+++ WebStore.Core/Contracts/Admin/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Core.Model.Admin.Product;
+using WebStore.Core.Model.Product;
+
+namespace WebStore.Core.Contracts.Admin
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductTableModel>> GetAllProductsAsync();
+
+    }
+}
---
+++ WebStore.Core/Services/Admin/ProductService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Core.Contracts.Admin;
+using WebStore.Core.Model.Admin.Product;
+using WebStore.Core.Model.Product;
+
+namespace WebStore.Core.Services.Admin
+{
+    public class ProductService : IProductService
+    {
+        public Task<IEnumerable<ProductTableModel>> GetAllProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
