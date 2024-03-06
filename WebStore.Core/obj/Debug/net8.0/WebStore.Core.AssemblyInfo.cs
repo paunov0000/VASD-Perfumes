60c4802dc262b91fd7aae94830329a398d54accc
paunov0000 <legendarygriefer96@gmail.com>
2024-03-06 23:05:34 +0200

    fixed css for .card cuz the elements of the card are wrapped in an anchor

--- WebStore.Core/obj/Debug/net8.0/WebStore.Core.AssemblyInfo.cs
+++ WebStore.Core/obj/Debug/net8.0/WebStore.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebStore.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9efe3fbc8b36fdaf94f4d1287951997917b920b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d54ef7c45c46e83bba7ebbb729d49bb713b8f00f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebStore.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebStore.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebStore.MVC/obj/Debug/net8.0/WebStore.MVC.AssemblyInfo.cs
+++ WebStore.MVC/obj/Debug/net8.0/WebStore.MVC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9efe3fbc8b36fdaf94f4d1287951997917b920b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5ca7ac35bb4b84d50c251736cad50862039f8c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
