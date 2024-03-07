397764bb3a0836e217516098af1218d7aea8d21d
paunov0000 <legendarygriefer96@gmail.com>
2024-03-07 23:14:10 +0200

    Renamed the ProductCategoryConfiguration to CategoryConfiguration due to changed business logic

---
+++ WebStore.Infrastructure/Data/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Data.Entities;
+using static WebStore.Infrastructure.Data.Constants.ConfigurationConstants.Category;
+
+namespace WebStore.Infrastructure.Data.Configuration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(CreateCategories());
+        }
+
+        private List<Category> CreateCategories()
+        {
+            return new List<Category>()
+            {
+                new Category
+                {
+                    Id = Guid.Parse(FragranceId),
+                    Name = "Fragrances",
+                },
+                //new Category
+                //{
+                //    Id = Guid.Parse(OrientalId),
+                //    Name = "Oriental",
+                //},
+                //new Category
+                //{
+                //    Id = Guid.Parse(FreshId),
+                //    Name = "Fresh",
+                //},
+                //new Category
+                //{
+                //    Id = Guid.Parse(WoodyId),
+                //    Name = "Woody",
+                //}
+            };
+        }
+    }
+}
