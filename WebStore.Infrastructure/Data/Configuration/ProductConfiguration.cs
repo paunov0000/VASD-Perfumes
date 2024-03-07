3c9a54b0f473cca2203bd7240d359ec70fd724f3
paunov0000 <legendarygriefer96@gmail.com>
2024-03-07 23:18:24 +0200

    Made changes to ProductConfiguration, Product Entity has a new property BrandId which is being seeded here, CategoryId is set to a constant which FragranceId because I'm seeding with fragrances only atm in the config

--- WebStore.Infrastructure/Data/Configuration/ProductConfiguration.cs
+++ WebStore.Infrastructure/Data/Configuration/ProductConfiguration.cs
@@ -2,7 +2,8 @@
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using WebStore.Infrastructure.Data.Entities;
 using static WebStore.Infrastructure.Data.Constants.ConfigurationConstants.Product;
-using static WebStore.Infrastructure.Data.Constants.ConfigurationConstants.ProductCategory;
+using static WebStore.Infrastructure.Data.Constants.ConfigurationConstants.Category;
+using static WebStore.Infrastructure.Data.Constants.ConfigurationConstants.Brand;
 
 namespace WebStore.Infrastructure.Data.Configuration
 {
@@ -28,112 +29,112 @@ namespace WebStore.Infrastructure.Data.Configuration
                 new Product
                 {
                     Id = Guid.Parse(Chanel05Id),
-                    Name = "Chanel No. 5",
+                    Name = "No. 5",
                     Description = "Iconic blend of rose and jasmine, a classic from luxury brand Chanel",
-                    Manufacturer = "Chanel",
                     Price = 120.00M,
                     ImageUrl ="https://www.sephora.com/productimages/sku/s465690-main-zoom.jpg?imwidth=612",
                     CreatedOn = DateTime.UtcNow,
-                    ProductCategoryId = Guid.Parse(FloralId)
+                    CategoryId = Guid.Parse(FragranceId),
+                    BrandId = Guid.Parse(ChanelId)
                 },
                 new Product
                 {
                     Id = Guid.Parse(MarcJacobsDaisyId),
-                    Name = "Marc Jacobs Daisy",
+                    Name = "Daisy",
                     Description = "Youthful and fresh floral scent with notes of jasmine and violet",
-                    Manufacturer = "Marc Jacobs",
                     Price = 80.00M,
                     ImageUrl = "https://www.sephora.com/productimages/sku/s1029958-main-zoom.jpg?imwidth=612",
                     CreatedOn = DateTime.UtcNow,
-                    ProductCategoryId = Guid.Parse(FloralId)
+                    CategoryId = Guid.Parse(FragranceId),
+                    BrandId = Guid.Parse(MarcJacobsId)
                 },
                 new Product
                 {
                     Id = Guid.Parse(GucciBloomId),
-                    Name = "Gucci Bloom",
+                    Name = "Bloom",
                     Description = "Modern and vibrant floral bouquet featuring tuberose and jasmine",
-                    Manufacturer = "Gucci",
                     Price = 150.00M,
                     ImageUrl = "https://www.sephora.com/productimages/sku/s1964832-main-zoom.jpg?imwidth=612",
                     CreatedOn = DateTime.UtcNow,
-                    ProductCategoryId = Guid.Parse(FloralId)
+                    CategoryId = Guid.Parse(FragranceId),
+                    BrandId = Guid.Parse(GucciId)
                 },
                 new Product
                 {
                     Id = Guid.Parse(ViktorRolfFlowerbombId),
-                    Name = "Viktor&Rolf Flowerbomb",
+                    Name = "Flowerbomb",
                     Description = "Intensely floral with notes of jasmine, rose, and orchid",
-                    Manufacturer = "Viktor&Rolf",
                     Price = 110.00M,
                     ImageUrl = "https://www.sephora.com/productimages/sku/s1377159-main-zoom.jpg?imwidth=612",
                     CreatedOn = DateTime.UtcNow,
-                    ProductCategoryId = Guid.Parse(FloralId)
+                    CategoryId = Guid.Parse(FragranceId),
+                    BrandId = Guid.Parse(ViktorRolfId)
                 },
                 new Product
                 {
                     Id = Guid.Parse(ShalimarByGuerlainId),
-                    Name = "Shalimar by Guerlain",
+                    Name = "Shalimar",
                     Description = "Timeless oriental scent with vanilla, iris, and amber notes",
-                    Manufacturer = "Guerlain",
                     Price = 140.00M,
                     ImageUrl = "https://douglas.bg/media/catalog/product/cache/dd4850ad4231b6306bceadf38a0bbeed/1/_/1_4439.jpg",
                     CreatedOn = DateTime.UtcNow,
-                    ProductCategoryId = Guid.Parse(OrientalId)
+                    CategoryId = Guid.Parse(FragranceId),
+                    BrandId = Guid.Parse(GuerlainId)
                 },
                 new Product
                 {
                     Id = Guid.Parse(OpiumByYvesSaintLaurentId),
-                    Name = "Opium by Yves Saint Laurent",
+                    Name = "Opium",
                     Description = "Rich and spicy oriental fragrance with exotic undertones",
-                    Manufacturer = "Yves Saint Laurent",
                     Price = 100.00M,
                     ImageUrl = "https://www.yslbeautyus.com/dw/image/v2/AANG_PRD/on/demandware.static/-/Sites-ysl-master-catalog/default/dwfd20b6ef/Fragrance/Fragrance/Opium_Eau_De_Toilette_Spray/3365440556386_Opium-Eau-De-Tpilette-Spray_01.jpg?sw=698&sh=698&sm=cut&sfrm=jpg&q=85",
                     CreatedOn = DateTime.UtcNow,
-                    ProductCategoryId = Guid.Parse(FreshId)
+                    CategoryId = Guid.Parse(FragranceId),
+                    BrandId = Guid.Parse(YvesSaintLaurentId)
                 },
                 new Product
                 {
                     Id = Guid.Parse(BlackOrchidByTomFordId),
-                    Name = "Black Orchid by Tom Ford",
+                    Name = "Black Orchid",
                     Description = "Luxurious blend of black truffle, vanilla, and orchid",
-                    Manufacturer = "Tom Ford",
                     Price = 180.00M,
                     ImageUrl = "https://www.sephora.com/productimages/sku/s1007731-main-zoom.jpg?imwidth=612",
                     CreatedOn = DateTime.UtcNow,
-                    ProductCategoryId = Guid.Parse(FreshId)
+                    CategoryId = Guid.Parse(FragranceId),
+                    BrandId = Guid.Parse(TomFordId)
                 },
                 new Product
                 {
                     Id = Guid.Parse(AcquaDiGioByGiorgioArmaniId),
-                    Name = "Acqua di Gio by Giorgio Armani",
+                    Name = "Acqua di Gio",
                     Description = "Fresh and aquatic scent with notes of citrus and rosemary",
-                    Manufacturer = "Giorgio Armani",
                     Price = 85.00M,
                     ImageUrl = "https://www.sephora.com/productimages/sku/s397299-main-zoom.jpg?imwidth=612",
                     CreatedOn = DateTime.UtcNow,
-                    ProductCategoryId = Guid.Parse(FreshId)
+                    CategoryId = Guid.Parse(FragranceId),
+                    BrandId = Guid.Parse(ArmaniId)
                 },
                 new Product
                 {
                     Id = Guid.Parse(TerreDhermesByHermesId),
-                    Name = "Terre d'Hermès by Hermès",
+                    Name = "Terre d'Hermès",
                     Description = "Citrusy and woody fragrance with notes of grapefruit and cedar",
-                    Manufacturer = "Hermès",
                     Price = 120.00M,
                     ImageUrl = "https://www.sephora.com/productimages/sku/s915447-main-zoom.jpg?imwidth=612",
                     CreatedOn = DateTime.UtcNow,
-                    ProductCategoryId = Guid.Parse(WoodyId)
+                    CategoryId = Guid.Parse(FragranceId),
+                    BrandId = Guid.Parse(HermesId)
                 },
                 new Product
                 {
                     Id = Guid.Parse(AventusByCreedId),
-                    Name = "Aventus by Creed",
+                    Name = "Aventus",
                     Description = "Fresh and fruity scent with notes of pineapple and blackcurrant",
-                    Manufacturer = "Creed",
                     Price = 250.00M,
                     ImageUrl = "https://creedboutique.com/cdn/shop/files/aventus-100ml-bottle_3413e5f4-3eee-40b3-8451-2546a370ec5b.jpg?v=1700498936&width=1500",
                     CreatedOn = DateTime.UtcNow,
-                    ProductCategoryId = Guid.Parse(WoodyId)
+                    CategoryId = Guid.Parse(FragranceId),
+                    BrandId = Guid.Parse(CreedId)
                 }
             };
         }
