c6dd6db44ea62d7fd819849bb1fa9ba050f9ba49
paunov0000 <legendarygriefer96@gmail.com>
2024-03-07 23:19:57 +0200

    Added Brand constants static class and updated the ex ProductCategory static class

--- WebStore.Infrastructure/Data/Constants/ConfigurationConstants.cs
+++ WebStore.Infrastructure/Data/Constants/ConfigurationConstants.cs
@@ -22,12 +22,26 @@ namespace WebStore.Infrastructure.Data.Constants
             public const string AventusByCreedId = "c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b66";
         }
 
-        public static class ProductCategory
+        public static class Category
         {
-            public const string FloralId = "53146915-6199-44eb-aedb-e9902299be6c";
-            public const string OrientalId = "53146915-6199-44eb-aedb-e9902299be6d";
-            public const string FreshId = "53146915-6199-44eb-aedb-e9902299be6e";
-            public const string WoodyId = "53146915-6199-44eb-aedb-e9902299be6f";
+            public const string FragranceId = "53146915-6199-44eb-aedb-e9902299be6c";
+            //public const string OrientalId = "53146915-6199-44eb-aedb-e9902299be6d";
+            //public const string FreshId = "53146915-6199-44eb-aedb-e9902299be6e";
+            //public const string WoodyId = "53146915-6199-44eb-aedb-e9902299be6f";
+        }
+
+        public static class Brand
+        {
+            public const string ChanelId = "361f69c3-cdfb-4156-a88a-1620db540c6c";
+            public const string MarcJacobsId = "c3a935d2-8a70-40f4-aa02-7dc52d4688ed";
+            public const string GucciId = "6c86c626-bef8-4627-a314-de021502e335";
+            public const string ViktorRolfId = "99f87405-5144-46e6-ad55-724d05928736";
+            public const string GuerlainId = "586011c7-d606-4e73-a7e8-fca768656c03";
+            public const string YvesSaintLaurentId = "57d95637-ffa7-4109-9d26-03bae7e0e6e1";
+            public const string TomFordId = "28f3d7f5-3469-4036-b919-ec683688314b";
+            public const string ArmaniId = "dc6ccd03-c8c8-47a8-b9fe-83e878b158f8";
+            public const string HermesId = "dc4a85cb-8f2d-452b-8027-c3409538c244";
+            public const string CreedId = "f33371d7-fa64-42f0-89bd-7f707e285279";
         }
     }
 }
