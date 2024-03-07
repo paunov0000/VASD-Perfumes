dc80e21d928a6cb6ed13ef13cea768c417bde7dd
paunov0000 <legendarygriefer96@gmail.com>
2024-03-07 23:43:09 +0200

    Added Brand Entity which has one-to-many relationship with the Product Entity

---
+++ WebStore.Infrastructure/Data/Entities/Brand.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static WebStore.Infrastructure.Data.Constants.ModelConstants.Brand;
+
+namespace WebStore.Infrastructure.Data.Entities
+{
+    [Comment("Holds info for the Brand entity")]
+    public class Brand
+    {
+        [Key]
+        [Comment("The unique identifier for the brand.")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(NameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [Comment("The products that belong to the brand.")]
+        public IEnumerable<Product> Products { get; set; } = new List<Product>();
+
+
+    }
+}
