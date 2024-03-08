0bd0fd0145655c541f2a10818b47f8dfdeb2a873
paunov0000 <legendarygriefer96@gmail.com>
2024-03-08 14:07:24 +0200

    Added methods for managing the Products Table

--- WebStore.Core/Contracts/Admin/IProductManageService.cs
+++ WebStore.Core/Contracts/Admin/IProductManageService.cs
@@ -1,16 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WebStore.Core.Model.Admin.Product;
+﻿using WebStore.Core.Model.Admin.Product;
 using WebStore.Core.Model.Product;
 
 namespace WebStore.Core.Contracts.Admin
 {
     public interface IProductManageService
     {
+
+        /// <summary>
+        /// Fetches all Product entities from the database and maps them to an IEnumerable of ProductTableModel
+        /// </summary>
+        /// <returns>An IEnumerable of ProductTableModel</returns>
         Task<IEnumerable<ProductTableModel>> GetAllProductsAsync();
 
+        /// <summary>
+        /// Fetches all Category entities from the database and maps them to an IEnumerable of CategoryFormViewModel
+        /// </summary>
+        /// <returns>An IEnumerable of CategoryFormViewModel</returns>
+        Task<IEnumerable<CategoryFormViewModel>> GetAllCategories();
+
+
+        /// <summary>
+        /// Fetches all Brand entities from the database and maps them to an IEnumerable of BrandFormViewModel
+        /// </summary>
+        /// <returns>An IEnumerable of BrandFormViewModel</returns>
+        Task<IEnumerable<BrandFormViewModel>> GetAllBrands();
+
+        /// <summary>
+        /// Adds a new Product entity to the database
+        /// </summary>
+        /// <param name="model">View model which holds the data</param>
+        Task AddProductAsync(ProductAddViewModel model);
+
+        /// <summary>
+        /// Edits/Updates a Product entity in the database
+        /// </summary>
+        /// <param name="model">A viewmodel which holds the data</param>
+        Task EditProductAsync(ProductAddViewModel model);
+
+        /// <summary>
+        /// Marks a Product entity from the database as inactive
+        /// </summary>
+        /// <param name="id">Unique identifier of the Entity performing actions on</param>
+        Task DeleteProductAsync(Guid id);
     }
 }
