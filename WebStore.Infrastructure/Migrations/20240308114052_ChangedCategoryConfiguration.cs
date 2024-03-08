13d67a0aafe854e276a292197c6d5a72dcd3ef62
paunov0000 <legendarygriefer96@gmail.com>
2024-03-08 14:12:14 +0200

    migrations which applied the new CategoryConfiguration

---
+++ WebStore.Infrastructure/Migrations/20240308114052_ChangedCategoryConfiguration.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebStore.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedCategoryConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("753efde4-efa1-4f88-abc9-8f091cf8b670"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "6884e0c8-2f18-4d1f-969c-a85e2efcca9f", "AQAAAAIAAYagAAAAEJnjZBVbH7eai9pJfv6RzEJqIals6tfVvpf4uy/igb8/ctmys76JoiuzwtEzmHjyoA==" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6c"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "90c4a3d1-0b4c-4dda-97fc-3ed3f4a917f5", "AQAAAAIAAYagAAAAEKzAOUqDvKzoRnlqbxY43XyEBOaNIDHsTq17+ZmFAEcKWON9ozO6VgR5lfdstW5iaw==" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("53146915-6199-44eb-aedb-e9902299be6c"),
+                column: "Name",
+                value: "Fragrance");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b66"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 8, 11, 40, 51, 822, DateTimeKind.Utc).AddTicks(9718));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b67"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 8, 11, 40, 51, 822, DateTimeKind.Utc).AddTicks(9714));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b68"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 8, 11, 40, 51, 822, DateTimeKind.Utc).AddTicks(9711));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b69"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 8, 11, 40, 51, 822, DateTimeKind.Utc).AddTicks(9708));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6a"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 8, 11, 40, 51, 822, DateTimeKind.Utc).AddTicks(9705));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6b"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 8, 11, 40, 51, 822, DateTimeKind.Utc).AddTicks(9701));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6c"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 8, 11, 40, 51, 822, DateTimeKind.Utc).AddTicks(9698));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6d"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 8, 11, 40, 51, 822, DateTimeKind.Utc).AddTicks(9694));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6e"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 8, 11, 40, 51, 822, DateTimeKind.Utc).AddTicks(9690));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6f"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 8, 11, 40, 51, 822, DateTimeKind.Utc).AddTicks(9683));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("753efde4-efa1-4f88-abc9-8f091cf8b670"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "464d2f7a-9f7f-49ed-8c4a-1f98e1f2c536", "AQAAAAIAAYagAAAAELlAWXYyv47xui2dS8Cssv3WNpSQPnGEB9m6E7a5I6E2BLwumsGXZKuJam+okUzXzA==" });
+
+            migrationBuilder.UpdateData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6c"),
+                columns: new[] { "ConcurrencyStamp", "PasswordHash" },
+                values: new object[] { "69f89b13-cd85-4736-bd15-5e14f2b816a7", "AQAAAAIAAYagAAAAEKxn7BYMoFCATRYs0cI4Vseee5spD3Z9lQOh3mW3Lj8i4OmVpQpfo9mAKGRmvABjCw==" });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("53146915-6199-44eb-aedb-e9902299be6c"),
+                column: "Name",
+                value: "Fragrances");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b66"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 7, 23, 45, 19, 231, DateTimeKind.Utc).AddTicks(4139));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b67"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 7, 23, 45, 19, 231, DateTimeKind.Utc).AddTicks(4136));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b68"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 7, 23, 45, 19, 231, DateTimeKind.Utc).AddTicks(4133));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b69"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 7, 23, 45, 19, 231, DateTimeKind.Utc).AddTicks(4130));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6a"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 7, 23, 45, 19, 231, DateTimeKind.Utc).AddTicks(4126));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6b"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 7, 23, 45, 19, 231, DateTimeKind.Utc).AddTicks(4122));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6c"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 7, 23, 45, 19, 231, DateTimeKind.Utc).AddTicks(4119));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6d"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 7, 23, 45, 19, 231, DateTimeKind.Utc).AddTicks(4116));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6e"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 7, 23, 45, 19, 231, DateTimeKind.Utc).AddTicks(4112));
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: new Guid("c0a0d5a0-4b6a-4b6a-8f4a-0c8f0b6f0b6f"),
+                column: "CreatedOn",
+                value: new DateTime(2024, 3, 7, 23, 45, 19, 231, DateTimeKind.Utc).AddTicks(4104));
+        }
+    }
+}
