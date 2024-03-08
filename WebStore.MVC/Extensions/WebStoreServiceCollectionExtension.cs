2544aef92dc639ba203f027a70570e9a9b4bcae2
paunov0000 <legendarygriefer96@gmail.com>
2024-03-09 01:03:00 +0200

    Added a ServiceCollectionExtension in order to clean up the Program.cs. I've added 2 method in side of the extension. AddApplicationIdentity<TUser> which configures the Identity by using the values in the appsettings.json.X and X references the current Environment. The second method AddApplicationServices takes care of the DI.

---
+++ WebStore.MVC/Extensions/WebStoreServiceCollectionExtension.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WebStore.Core.Contracts;
+using WebStore.Core.Contracts.Admin;
+using WebStore.Core.Misc;
+using WebStore.Core.Services;
+using WebStore.Core.Services.Admin;
+using WebStore.Infrastructure.Common;
+using WebStore.Infrastructure.Data;
+using WebStore.Infrastructure.Data.Entities;
+
+namespace Microsoft.Extensions.DependencyInjection
+{ 
+    public static class WebStoreServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IRepository, Repository>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IProductManageService, ProductManageService>();
+
+            return services;
+        }
+
+        public static IdentityBuilder AddApplicationIdentity<TUser>(this IServiceCollection services, IConfiguration configuration) where TUser : class
+        {
+            services.AddDefaultIdentity<TUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedAccount");
+                options.SignIn.RequireConfirmedEmail = configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedEmail");
+                options.SignIn.RequireConfirmedPhoneNumber = configuration.GetValue<bool>("Identity:SignIn:RequireConfirmedPhoneNumber");
+                options.Password.RequiredLength = configuration.GetValue<int>("Identity:Password:RequiredLength");
+                options.Password.RequireNonAlphanumeric = configuration.GetValue<bool>("Identity:Password:RequireNonAlphanumeric");
+                options.Password.RequireUppercase = configuration.GetValue<bool>("Identity:Password:RequireUppercase");
+                options.Password.RequireLowercase = configuration.GetValue<bool>("Identity:Password:RequireLowercase");
+                options.Password.RequireDigit = configuration.GetValue<bool>("Identity:Password:RequireDigit");
+            })
+                .AddRoles<ApplicationRole>()
+                .AddEntityFrameworkStores<WebStoreDbContext>()
+                .AddDefaultTokenProviders()
+                .AddUserManager<UserManager<TUser>>()
+                .AddErrorDescriber<CustomIdentityErrorDescriber>();
+
+            return new IdentityBuilder(typeof(TUser), services);
+        }
+    }
+}
