997b7ff91022a37f7d8ee47d33a081257f103b45
paunov0000 <legendarygriefer96@gmail.com>
2024-03-08 14:14:17 +0200

    Added Actions for the Admin area of the Product controller, Injected IProductService to use its` GetProductByIdAsync method

--- WebStore.MVC/Areas/Admin/Controllers/ProductController.cs
+++ WebStore.MVC/Areas/Admin/Controllers/ProductController.cs
@@ -1,15 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebStore.Core.Contracts.Admin;
+using static WebStore.Core.Constants.TempDataKeyConstants;
+using WebStore.Core.Model.Product;
+using WebStore.Core.Services;
+using WebStore.Core.Contracts;
 
 namespace WebStore.MVC.Areas.Admin.Controllers
 {
     public class ProductController : BaseController
     {
         private readonly IProductManageService productManageService;
+        private readonly IProductService productService;
 
-        public ProductController(IProductManageService _productManageService)
+        public ProductController(
+                                IProductManageService _productManageService,
+                                IProductService _productService
+                                )
         {
             productManageService = _productManageService;
+            productService = _productService;
         }
 
         [HttpGet]
@@ -18,6 +27,96 @@ namespace WebStore.MVC.Areas.Admin.Controllers
             var model = await productManageService.GetAllProductsAsync();
 
             return View(model);
+        }
+
+        //[Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var model = new ProductAddViewModel();
+
+            ViewBag.Categories = await productManageService.GetAllCategories();
+            ViewBag.Brands = await productManageService.GetAllBrands();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProductAddViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Categories = await productManageService.GetAllCategories();
+                ViewBag.Brands = await productManageService.GetAllBrands();
+
+                TempData[Status.Error] = Product.InvalidInputMessage;
+
+                return View(model);
+            }
+
+            await productManageService.AddProductAsync(model);
+
+            TempData[Status.Success] = Product.SuccessOnAddMessage;
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            try
+            {
+                var model = await productService.GetProductByIdAsync(id);
+
+                ViewBag.ProductCategories = await productManageService.GetAllCategories();
+
+                return View(model);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductAddViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.ProductCategories = await productManageService.GetAllCategories();
+
+                TempData[Status.Error] = Product.InvalidInputMessage;
+
+                return View(model);
+            }
+
+            try
+            {
+                await productManageService.EditProductAsync(model);
+
+                TempData[Status.Success] = Product.SuccessOnEditMessage;
+
+                return RedirectToAction("Index", "Product");
+            }
+            catch (InvalidOperationException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                await productManageService.DeleteProductAsync(id);
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (InvalidOperationException ex)
+            {
+                return NotFound(ex.Message);
+            }
         }
     }
 }
