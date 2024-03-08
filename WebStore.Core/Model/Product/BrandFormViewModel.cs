e8203a8720fed9bc2f97e1a0ac281ce957d96c7f
paunov0000 <legendarygriefer96@gmail.com>
2024-03-08 14:09:01 +0200

    removed unused usings

--- WebStore.Core/Model/Product/BrandFormViewModel.cs
+++ WebStore.Core/Model/Product/BrandFormViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebStore.Core.Model.Product
+﻿namespace WebStore.Core.Model.Product
 {
     public class BrandFormViewModel
     {
