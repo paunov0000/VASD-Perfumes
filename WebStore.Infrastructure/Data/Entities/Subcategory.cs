f38496df8e9df273dc1b24c4402fd14f7d79a00d
paunov0000 <legendarygriefer96@gmail.com>
2024-03-10 15:42:37 +0200

    Created Subcategory Entity with Many-To-One relation to Category

---
+++ WebStore.Infrastructure/Data/Entities/Subcategory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebStore.Infrastructure.Data.Entities
+{
+    [Comment("Holds info for the Product Subcategory entity")]
+    public class Subcategory
+    {
+        [Key]
+        [Comment("Primary key of the product subcategory")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [Comment("Name of the product subcategory")]
+        public string Name { get; set; } = null!;
+
+        [ForeignKey(nameof(Category))]
+        [Comment("Foreign key to the Category entity")]
+        public Guid CategoryId { get; set; }
+
+        [Required]
+        public Category Category { get; set; } = null!;
+    }
+}
