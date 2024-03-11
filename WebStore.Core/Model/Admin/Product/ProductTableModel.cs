3486f0323993edf4288d9c4607bd11a4d0b5463d
paunov0000 <legendarygriefer96@gmail.com>
2024-03-11 18:32:55 +0200

    Added Display attribute on some properties to make the names appropriate for the views

--- WebStore.Core/Model/Admin/Product/ProductTableModel.cs
+++ WebStore.Core/Model/Admin/Product/ProductTableModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using WebStore.Infrastructure.Data.Entities;
 
 namespace WebStore.Core.Model.Admin.Product
@@ -9,17 +10,24 @@ namespace WebStore.Core.Model.Admin.Product
 
         public string Name { get; set; } = null!;
 
+        [Display(Name = "Brand")]
         public string BrandName { get; set; } = null!;
 
         public decimal Price { get; set; }
 
+        [Display(Name = "Sold Count")]
         public int SoldCount { get; set; }
 
         public int Quantity { get; set; }
 
+        [Display(Name = "On Sale")]
         public bool OnSale { get; set; }
 
+        [Display(Name = "Category")]
         public string CategoryName { get; set; } = null!;
+
+        [Display(Name = "Subcategory")]
+        public string SubcategoryName { get; set; } = null!;
 
 
     }
