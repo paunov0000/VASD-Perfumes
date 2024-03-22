03ecb67a0bdaf9b4ae716f99cf2fc7f2013533da
paunov0000 <legendarygriefer96@gmail.com>
2024-03-22 21:40:39 +0200

    Added a ViewModel for the subcategory which will be displayed in a select element, therefore I only need Id and Name

---
+++ WebStore.Core/Model/Admin/Product/SubcategoryFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Core.Model.Admin.Product
+{
+    public class SubcategoryFormModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
