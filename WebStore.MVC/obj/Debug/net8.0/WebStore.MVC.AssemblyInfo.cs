e46603bb54afd734f948dfe4b5831b6e5af198d1
paunov0000 <legendarygriefer96@gmail.com>
2024-03-29 00:44:24 +0200

    build

--- WebStore.MVC/obj/Debug/net8.0/WebStore.MVC.AssemblyInfo.cs
+++ WebStore.MVC/obj/Debug/net8.0/WebStore.MVC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4612b3bd83e3846d61cc6d487f239c9bf66c5767")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d2dea00cd174a0fca1158473093dcd5eecf8291")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
