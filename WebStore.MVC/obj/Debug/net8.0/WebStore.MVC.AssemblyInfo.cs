9b5d7a38c6cb34eb9f87b200d33748c2bcd1e3e8
paunov0000 <legendarygriefer96@gmail.com>
2024-04-01 14:45:51 +0300

    Added the newly created AdminLayout, must add some razor tag helpers and that will be for the layout

--- WebStore.MVC/obj/Debug/net8.0/WebStore.MVC.AssemblyInfo.cs
+++ WebStore.MVC/obj/Debug/net8.0/WebStore.MVC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7827d22bf87a58c51f7eb79f2b0d065979d31e57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+662fcfe7a0154229d185d7ee278f8857b11d9086")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
