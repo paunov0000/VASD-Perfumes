abca537cfd7bc2f68594c8eba724684fadd1e24d
paunov0000 <legendarygriefer96@gmail.com>
2024-04-04 00:42:39 +0300

    Created model for Product/Restock endpoint

---
+++ WebStore.Core/Model/Product/ProductRestockViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Core.Model.Product
+{
+    public class ProductRestockViewModel
+    {
+        [Display(Name = "Product | Quantity")]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Restock Count")]
+        public int Count { get; set; }
+    }
+}
