0eb23b971b79ec625e642810e7dd4337f19ddd85
paunov0000 <legendarygriefer96@gmail.com>
2024-04-05 20:02:02 +0300

    Added a UserController with Index, Details, Promote, Demote and Ban actions

---
+++ WebStore.MVC/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Core.Contracts.Admin;
+
+namespace WebStore.MVC.Areas.Admin.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly IUserManageService userManageService;
+
+        public UserController(IUserManageService _userManageService)
+        {
+            userManageService = _userManageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = await userManageService.GetAllUsersAsync();
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var model = await userManageService.GetUserByIdAsync(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Promote(Guid id)
+        {
+            try
+            {
+                await userManageService.PromoteUserAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NullReferenceException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Demote(Guid id)
+        {
+            try
+            {
+                await userManageService.DemoteUserAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NullReferenceException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Ban(Guid id)
+        {
+            try
+            {
+                await userManageService.BanUserAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NullReferenceException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
