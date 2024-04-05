43b6c7931a009a938b1361ce6086b4c7c2870850
paunov0000 <legendarygriefer96@gmail.com>
2024-04-05 20:07:04 +0300

    Added UserTableModel which will be a model displayed in table format. It contains the main properties from the ApplicationUser entity

---
+++ WebStore.Core/Model/Admin/User/UserTableModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Core.Model.Admin.User
+{
+    public class UserTableModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = null!; //TODO: 1st name + last name
+
+        public string? UserName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+    }
+}
