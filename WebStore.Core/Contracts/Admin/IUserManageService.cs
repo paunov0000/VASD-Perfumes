a1a2a31a274b2784fb53123f22395417ea571148
paunov0000 <legendarygriefer96@gmail.com>
2024-04-05 19:50:22 +0300

    Added IUserManageService

---
+++ WebStore.Core/Contracts/Admin/IUserManageService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Core.Model.Admin.User;
+
+namespace WebStore.Core.Contracts.Admin
+{
+    public interface IUserManageService
+    {
+        public Task<IEnumerable<UserTableModel>> GetAllUsersAsync();
+
+        public Task<UserDetailsModel> GetUserByIdAsync(Guid id);
+
+        public Task PromoteUserAsync(Guid id);
+
+        public Task DemoteUserAsync(Guid id);
+
+        public Task BanUserAsync(Guid id);
+    }
+}
