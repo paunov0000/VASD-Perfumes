090612fef6b12dcf44453f70bcd7d27a9511460c
paunov0000 <legendarygriefer96@gmail.com>
2024-04-06 23:22:08 +0300

    Added necessary properties for the details view

--- WebStore.Core/Model/Product/ProductDetailsViewModel.cs
+++ WebStore.Core/Model/Product/ProductDetailsViewModel.cs
@@ -8,5 +8,14 @@ namespace WebStore.Core.Model.Product
 {
     public class ProductDetailsViewModel
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string ProductImg { get; set; } = null!;
+        public string BrandImg { get; set; } = null!;
+        public decimal Price { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public string SubcategoryName { get; set; } = null!;
+
     }
 }
