5b092e5acec6b02f7c72964c368e633e9da88af0
paunov0000 <legendarygriefer96@gmail.com>
2024-04-06 12:34:13 +0300

    Added Roles property, an IEnumerable of strings

--- WebStore.Core/Model/Admin/User/UserTableModel.cs
+++ WebStore.Core/Model/Admin/User/UserTableModel.cs
@@ -18,5 +18,7 @@ namespace WebStore.Core.Model.Admin.User
 
         public string? PhoneNumber { get; set; }
 
+        public IEnumerable<string> Roles { get; set; } = null!; //TODO: when i fetch the roles if a user has no roles it has to return an empty list, right?
+
     }
 }
