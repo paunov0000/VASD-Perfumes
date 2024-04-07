243159c7be2ba6a4cbef1ff2dccecf1511792fce
paunov0000 <legendarygriefer96@gmail.com>
2024-04-07 18:44:57 +0300

    Revert "Reapply "Added roles to certain users in the configuration""
    This reverts commit 9a8c8e256cc1ffb7ef1e044b0cab947d8a53709e.

--- WebStore.MVC/obj/Debug/net8.0/WebStore.MVC.AssemblyInfo.cs
+++ WebStore.MVC/obj/Debug/net8.0/WebStore.MVC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43b6c7931a009a938b1361ce6086b4c7c2870850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0204c5f63fb5300bed1a8a94255402e0fa2aa602")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebStore.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
