1920c0347946a15fccfad8ed38af26637af0e0c3
paunov0000 <legendarygriefer96@gmail.com>
2024-04-08 16:23:54 +0300

    Added CartController, has default Index action

---
+++ WebStore.MVC/Controllers/CartController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Core.Model.Product;
+using static WebStore.Core.Constants.TempDataKeyConstants;
+
+namespace WebStore.MVC.Controllers
+{
+    public class CartController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
