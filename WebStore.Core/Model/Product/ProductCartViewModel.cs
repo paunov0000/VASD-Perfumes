72345502edd77b06fe08a79f64f247a7674289ff
paunov0000 <legendarygriefer96@gmail.com>
2024-04-08 16:22:11 +0300

    Added ProductCartViewModel to display each product added to the cart local storage. Cart/Index is the endpoint. TotalPrice is a calculated property, idk if its a good way of going about it

---
+++ WebStore.Core/Model/Product/ProductCartViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Core.Model.Product
+{
+    public class ProductCartViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                return Quantity * Price;
+            }
+        } // Quantity * Price
+    }
+}
