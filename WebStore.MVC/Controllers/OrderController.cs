711a65703c2b1c1315f26adb29a0a7340441f97a
paunov0000 <legendarygriefer96@gmail.com>
2024-04-08 17:06:42 +0300

    Added OrderController, on order completion it'll add a +1 to the count of orders of the customer

---
+++ WebStore.MVC/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.MVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
