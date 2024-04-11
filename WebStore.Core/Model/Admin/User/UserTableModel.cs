9475bc97fffc8ca34196c14a4c44f460a9d2992b
paunov0000 <legendarygriefer96@gmail.com>
2024-04-12 02:59:48 +0300

    Removed unnecessary properties

--- WebStore.Core/Model/Admin/User/UserTableModel.cs
+++ WebStore.Core/Model/Admin/User/UserTableModel.cs
@@ -10,10 +10,6 @@ namespace WebStore.Core.Model.Admin.User
     {
         public Guid Id { get; set; }
 
-        public string Name { get; set; } = null!; //TODO: 1st name + last name
-
-        public string? UserName { get; set; }
-
         public string? Email { get; set; }
 
         public string? PhoneNumber { get; set; }
