eef4e37da3753b5299b6a0293f14dba66e8f80a9
paunov0000 <legendarygriefer96@gmail.com>
2024-04-17 13:36:06 +0300

    Created OrderTableModel, will use it in /Admin/Order

---
+++ WebStore.Core/Model/Admin/Order/OrderTableModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Core.Model.Admin.Order
+{
+    public class OrderTableModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Customer")]
+        public string CustomerEmail { get; set; } = null!;
+
+        [Display(Name = "Created")]
+        public DateTime OrderDate { get; set; }
+
+        [Display(Name = "Status")]
+        public string OrderStatus { get; set; } = null!;
+
+        [Display(Name = "Total Price")]
+        public decimal TotalPrice { get; set; }
+    }
+}
