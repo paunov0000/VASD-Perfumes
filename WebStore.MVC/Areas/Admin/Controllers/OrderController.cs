2d16b35878d927d8d23f194856c8aa4cd13482d8
paunov0000 <legendarygriefer96@gmail.com>
2024-04-17 13:34:35 +0300

    Created OrderController. Added IOrderManageService to implement the Index action

---
+++ WebStore.MVC/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Core.Contracts.Admin;
+using WebStore.Core.Services.Admin;
+
+namespace WebStore.MVC.Areas.Admin.Controllers
+{
+    public class OrderController : BaseController
+    {
+        private readonly IOrderManageService orderManageService;
+
+        public OrderController(IOrderManageService _orderManageService)
+        {
+            orderManageService = _orderManageService;
+        }
+
+        public async Task<IActionResult> Index([FromQuery] int count = 0, [FromQuery] string sort = "date-asc")
+        {
+            var model = await orderManageService.GetAllOrders(count, sort);
+
+            return View(model);
+        }
+    }
+}
