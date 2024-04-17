e0c033c0e172f3708c3c4f332f4a862a9e8fce1b
paunov0000 <legendarygriefer96@gmail.com>
2024-04-17 13:30:44 +0300

    Added IOrderManageService with GetAllOrders method

---
+++ WebStore.Core/Contracts/Admin/IOrderManageService.cs
@@ -0,0 +1,13 @@
+﻿using WebStore.Core.Model.Admin.Order;
+
+namespace WebStore.Core.Contracts.Admin
+{
+    public interface IOrderManageService
+    {
+        /// <summary>
+        /// Fetches all Order entities from the database and maps them to an IEnumerable of OrderTableModel
+        /// </summary>
+        /// <returns>An IEnumerable of OrderTableModel</returns>
+        Task<IEnumerable<OrderTableModel>> GetAllOrders(int count, string sort);
+    }
+}
