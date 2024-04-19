a9aea92dc466f2da4fb116cbc05e465b9666e9f7
paunov0000 <legendarygriefer96@gmail.com>
2024-04-19 18:12:32 +0300

    Created OrderFormModel, seems like somethnig is missing though, will look into it later

---
+++ WebStore.Core/Model/Admin/Order/OrderFormModel.cs
@@ -0,0 +1,10 @@
+﻿using WebStore.Core.Model.Admin.Product;
+
+namespace WebStore.Core.Model.Admin.Order
+{
+    public class OrderFormModel
+    {
+        public int OrderStatusId { get; set; }
+        public IEnumerable<ProductOrderModel> Products { get; set; } = null!;
+    }
+}
