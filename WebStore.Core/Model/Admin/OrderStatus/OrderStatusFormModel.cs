600ff65bcf7b93d5638cc40f0f6a320d1d88a903
paunov0000 <legendarygriefer96@gmail.com>
2024-04-19 18:13:19 +0300

    Created OrderStatusFormModel

---
+++ WebStore.Core/Model/Admin/OrderStatus/OrderStatusFormModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Core.Model.Admin.OrderStatus
+{
+    public class OrderStatusFormModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+    }
+}
