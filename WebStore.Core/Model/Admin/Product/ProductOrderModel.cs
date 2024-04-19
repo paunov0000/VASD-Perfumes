3cda9850ae9afbd60fa2a7bfe6810e08bfb9c9a3
paunov0000 <legendarygriefer96@gmail.com>
2024-04-19 18:13:38 +0300

    Created ProductOrderModel

---
+++ WebStore.Core/Model/Admin/Product/ProductOrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Core.Model.Admin.Product
+{
+    public class ProductOrderModel
+    {
+        public string Name { get; set; } = null!;
+
+        public int Quantity { get; set; }
+    }
+}
