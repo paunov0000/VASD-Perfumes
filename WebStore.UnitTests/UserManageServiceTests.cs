cc6dc935584c605f1d6c1541a4e1c5b3409bde85
paunov0000 <legendarygriefer96@gmail.com>
2024-04-24 17:58:38 +0300

    Created a UserManageServiceTests file but i cba atm, have to mock user manager

---
+++ WebStore.UnitTests/UserManageServiceTests.cs
@@ -0,0 +1,55 @@
+﻿//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+//using Moq;
+//using WebStore.Core.Contracts.Admin;
+//using WebStore.Core.Services.Admin;
+//using WebStore.Infrastructure.Common;
+//using WebStore.Infrastructure.Data;
+
+//namespace WebStore.UnitTests
+//{
+//    [TestFixture]
+//    public class UserManageServiceTests
+//    {
+//        private IRepository repo;
+//        private IUserManageService userManageService;
+//        private WebStoreDbContext dbContext;
+
+
+//        [SetUp]
+//        public void Setup()
+//        {
+//            var options = new DbContextOptionsBuilder<WebStoreDbContext>()
+//                .UseInMemoryDatabase("UserDb")
+//                .Options;
+
+//            this.dbContext = new WebStoreDbContext(options);
+//            this.repo = new Repository(dbContext);
+
+//            this.dbContext.Database.EnsureDeleted();
+
+//            this.dbContext.Database.EnsureCreated();
+
+//            this.userManageService = new UserManageService(repo, );
+//        }
+
+//        [TearDown]
+//        public void TearDown()
+//        {
+//            this.dbContext.Dispose();
+//            this.repo.Dispose();
+//        }
+
+//        public static Mock<UserManager<TUser>> MockUserManager<TUser>(List<TUser> users)
+//            where TUser : class
+//        {
+//            var store = new Mock<IUserStore<TUser>>();
+//            var manager = new Mock<UserManager<TUser>>(store.Object);
+
+//            manager.Object.UserValidators.Add(new UserValidator<TUser>());
+//            manager.Object.PasswordValidators.Add(new PasswordValidator<TUser>());
+
+//            manager.Setup(x => x.AddToRoleAsync(It.IsAny<TUser>(), It.IsAny<string>()));
+//        }
+//    }
+//}
