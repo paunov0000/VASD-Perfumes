26cb1bcce24bceb556e893846d9f77ac3db3c342
paunov0000 <legendarygriefer96@gmail.com>
2024-04-24 17:59:04 +0300

    Removed some usings

--- WebStore.Core/Services/Admin/UserManageService.cs
+++ WebStore.Core/Services/Admin/UserManageService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WebStore.Infrastructure.Data.Entities;
+﻿using WebStore.Infrastructure.Data.Entities;
 using WebStore.Core.Contracts.Admin;
 using WebStore.Core.Model.Admin.User;
 using WebStore.Infrastructure.Common;
