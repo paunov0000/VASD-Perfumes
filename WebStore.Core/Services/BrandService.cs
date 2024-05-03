3d228410e888c0d96b0cf62193b66515e693fc72
paunov0000 <legendarygriefer96@gmail.com>
2024-05-03 22:53:47 +0300

    Created BrandService. Implements IBrandService interface. Utilized IoC to access Repository and implemented GetHomeBrands method using it

---
+++ WebStore.Core/Services/BrandService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.Core.Contracts;
+using WebStore.Core.Model.Brand;
+using WebStore.Infrastructure.Common;
+using WebStore.Infrastructure.Data.Entities;
+
+namespace WebStore.Core.Services
+{
+    public class BrandService : IBrandService
+    {
+        private readonly IRepository repo;
+
+        public BrandService(IRepository _repo)
+        {
+            repo = _repo;
+        }
+
+        public async Task<IEnumerable<HomeBrandViewModel>> GetHomeBrands(int count)
+        {
+            return await repo.AllReadonly<Brand>()
+                .Take(count)
+                .Select(b => new HomeBrandViewModel()
+                {
+                    Id = b.Id,
+                    ImageUrl = b.ImageUrl,
+                })
+                .ToListAsync();
+        }
+    }
+}
