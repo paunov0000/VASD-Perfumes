b859c93a27d74788ef705738445f95b94defde87
paunov0000 <legendarygriefer96@gmail.com>
2024-05-03 22:51:49 +0300

    Created HomeBrandViewModel to display brand images/logos in a carousel. Need the ID to redirect to all products of given Brand

---
+++ WebStore.Core/Model/Brand/HomeBrandViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WebStore.Core.Model.Brand
+{
+    public class HomeBrandViewModel
+    {
+        public Guid Id { get; set; }
+        public string ImageUrl { get; set; } = null!;
+    }
+}
