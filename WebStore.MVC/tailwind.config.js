ab3602b7487b6c90c52ef92b6dc146982f89535e
paunov0000 <legendarygriefer96@gmail.com>
2024-05-07 02:06:16 +0300

    Extended with a custom animation

--- WebStore.MVC/tailwind.config.js
+++ WebStore.MVC/tailwind.config.js
@@ -1,15 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./wwwroot/pages/*.html", "./Views/**/*.cshtml", "./Areas/**/*.cshtml"],
-  theme: {
-    extend: {
-      fontFamily: {
-        main: ["Montserrat", "sans-serif"],
-      },
-      width: {
-        "site-width": "1500px",
-      },
+    content: ["./wwwroot/pages/*.html", "./Views/**/*.cshtml", "./Areas/**/*.cshtml"],
+    theme: {
+        extend: {
+            fontFamily: {
+                main: ["Montserrat", "sans-serif"],
+            },
+            width: {
+                "site-width": "1500px",
+            },
+            keyframes: {
+                animatedgradient: {
+                    '0%': { backgroundPosition: '0% 50%' },
+                    '50%': { backgroundPosition: '100% 50%' },
+                    '100%': { backgroundPosition: '0% 50%' },
+                },
+            },
+            backgroundSize: {
+                '200%': '200%'
+            },
+            animation: {
+                gradient: 'animatedgradient 6s ease infinite alternate'
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
