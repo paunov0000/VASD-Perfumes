47db0f43e883a024cc890be5628b3d0ed1209606
paunov0000 <legendarygriefer96@gmail.com>
2024-05-19 15:37:16 +0300

    Created a ChildSubcategory

---
+++ WebStore.Infrastructure/Data/Entities/ChildSubcategory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebStore.Infrastructure.Data.Entities
+{
+    public class ChildSubcategory
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        [ForeignKey(nameof(ParentSubcategory))]
+        public Guid ParentSubcategoryId { get; set; }
+
+        [Required]
+        public ParentSubcategory ParentSubcategory { get; set; } = null!;
+    }
+}
